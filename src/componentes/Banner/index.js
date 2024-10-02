a93f1111dac51af56a0e9d413b358e323b11bb06
Livia-CA <liviacaetanodeandrade@gmail.com>
2024-10-02 17:38:14 -0400

    projeto organo

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner inicial"/>
+        </header>
+    )
+}
+
+export default Banner
